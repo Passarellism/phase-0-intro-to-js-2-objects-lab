c24f54694b45c06f07d51bc4507f2e5b9fad56d7
Steve Passarelli <passarellism@gmail.com>
2022-12-20 14:05:53 -0700

    commit tree 070ba910cbe8ca0172f2599c1c2d0554460cd548

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+const employee = {
+    name: "Steve",
+    address: "1732 Daisy Court",
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj;
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = { ...obj };
+    newObj[key] = value;
+
+    return newObj;
+}
+
+// trouble below this line
+//nondestructive 
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = { ...obj };
+    delete newObj[key];
+
+    return newObj;
+}
+//destructive
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+
+    return obj
+}
